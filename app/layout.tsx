7ce499e38065e2dc7a4df32617fb7c3b70253962
Ihor-Kovalenko13 <ihor.kovalenko.01@gmail.com>
2026-01-05 15:35:50 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import './globals.css';
+
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: 'NoteHub',
+  description: 'NoteHub is a simple and efficient application for managing notes.',
+  openGraph: {
+    title: 'NoteHub',
+    description:
+      'NoteHub is a simple and efficient application for managing personal notes.',
+    url: 'https://08-zustand-seven-xi.vercel.app/',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub',
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={roboto.variable}>
+        <TanStackProvider>
+          <Header />
+          {children}
+          {modal}
+          <Footer />
+        </TanStackProvider>
+      </body>
+    </html>
+  );
+}
