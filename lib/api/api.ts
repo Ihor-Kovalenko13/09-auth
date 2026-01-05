19e3f6d1c7cc402119891a0c6c91b76b043cef0e
Igor <ihor.kovalenko.01@gmail.com>
2026-01-05 17:01:34 +0100

    fix: add API methods

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import type { Note } from '@/types/note';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -6,3 +7,22 @@ export const api = axios.create({
   baseURL: `${baseURL}/api`,
   withCredentials: true,
 });
+
+export const fetchNotes = async () => {
+  const { data } = await api.get<Note[]>('/notes');
+  return data;
+};
+
+export const fetchNoteById = async (id: string) => {
+  const { data } = await api.get<Note>(`/notes/${id}`);
+  return data;
+};
+
+export const createNote = async (note: Pick<Note, 'title' | 'content'>) => {
+  const { data } = await api.post<Note>('/notes', note);
+  return data;
+};
+
+export const deleteNote = async (id: string) => {
+  await api.delete(`/notes/${id}`);
+};
